97f55311b0ff23a2036365f52c40c2f51cc4b063
Johnny Goth <Jgoth23@gmail.com>
2021-02-11 20:37:19 -0600

    postico created and db sql loaded

--- database.sql
+++ database.sql
@@ -3,3 +3,28 @@ CREATE TABLE "artists" (
     "artist_name" varchar(80) not null,
     "year_born" date
 );
+INSERT INTO "artist"
+   ("name", "birthdate")
+   
+VALUES
+   ('Ella Fitzgerald', '04-25-1917'),
+   ('Dave Brubeck', '12-06-1920'),
+   ('Miles Davis', '05-26-1926'),
+   ('Esperanza Spalding', '10-18-1984');
+   
+
+
+CREATE TABLE "song" (
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR(255),
+    "length" VARCHAR(10),
+    "released" DATE
+);
+
+INSERT INTO "song"
+   ("title", "length", "released")
+   
+VALUES
+   ('Take Five', '5:24', '1959-09-29'),
+   ('So What', '9:22', '1959-08-17'),
+   ('Black Gold', '5:17', '2012-02-01');
